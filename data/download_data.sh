9aeda544f10983436f9cf951f58747971959c8ba
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-11 14:14:06 +0000

    Script to download Medley-solos-DB

--- data/download_data.sh
+++ data/download_data.sh
@@ -4,7 +4,7 @@ if [[ ! -f ~/Medley-solos-DB.tar.gz ]]; then
     echo "Medley-solos-DB.tar.gz... Hold your horses, this might take a while!"
     wget https://zenodo.org/record/3464194/files/Medley-solos-DB.tar.gz?download=1
     mv Medley-solos-DB.tar.gz?download=1 ~/Medley-solos-DB.tar.gz
-    tar -zxvf ~/Medley-solos-DB.tar.gz ~/Medley-solos-DB/
+    tar -C ~/Medley-solos-DB -zxvf ~/Medley-solos-DB.tar.gz
 else
     echo "Medley-solos-DB.tar.gz already exists"
 fi
