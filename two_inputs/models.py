238f33b5bdf60fec198f4da152c1ebd9c006ddfa
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-30 15:56:39 +0000

    modify CNN_two with log(...

--- two_inputs/models.py
+++ two_inputs/models.py
@@ -6,6 +6,7 @@ import math
 from stft import Spectrogram, LogmelFilterBank
 from augmentation import SpecAugmentation
 
+
 def init_layer(layer):
     """Initialize a Linear or Convolutional layer. """
     nn.init.xavier_uniform_(layer.weight)
@@ -14,75 +15,156 @@ def init_layer(layer):
         if layer.bias is not None:
             layer.bias.data.fill_(0.0)
 
+
 def init_bn(bn):
     """Initialize a Batchnorm layer. """
     bn.bias.data.fill_(0.0)
     bn.weight.data.fill_(1.0)
 
+
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
+
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
-        
+
         super(ConvBlock, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=in_channels, 
-                              out_channels=out_channels,
-                              kernel_size=kernel_size, stride=(1, 1),
-                              padding=(1, 1), bias=False)
-                              
-        self.conv2 = nn.Conv2d(in_channels=out_channels, 
-                              out_channels=out_channels,
-                              kernel_size=kernel_size, stride=(1, 1),
-                              padding=(1, 1), bias=False)
-                              
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False,
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False,
+        )
+
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.init_weight()
-        
+
     def init_weight(self):
         init_layer(self.conv1)
         init_layer(self.conv2)
 
+    def forward(self, input, pool_size=(2, 2), pool_type="avg"):
 
-        
-    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
-        
         x = input
         x = F.relu_(self.bn1(self.conv1(x)))
         x = F.relu_(self.bn2(self.conv2(x)))
-        if pool_type == 'max':
+        if pool_type == "max":
             x = F.max_pool2d(x, kernel_size=pool_size)
-        elif pool_type == 'avg':
+        elif pool_type == "avg":
             x = F.avg_pool2d(x, kernel_size=pool_size)
-        elif pool_type == 'avg+max':
+        elif pool_type == "avg+max":
             x1 = F.avg_pool2d(x, kernel_size=pool_size)
             x2 = F.max_pool2d(x, kernel_size=pool_size)
             x = x1 + x2
         else:
-            raise Exception('Incorrect argument!')
-        
+            raise Exception("Incorrect argument!")
+
         return x
-    
+
+
+class ConvBlock5x5(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=(5, 5)):
+
+        super(ConvBlock5x5, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=(1, 1),
+            padding=(2, 2),
+            bias=False,
+        )
+
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.conv1)
+        init_bn(self.bn1)
+
+    def forward(self, input, pool_size=(2, 2), pool_type="avg"):
+
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        if pool_type == "max":
+            x = F.max_pool2d(x, kernel_size=pool_size)
+        elif pool_type == "avg":
+            x = F.avg_pool2d(x, kernel_size=pool_size)
+        elif pool_type == "avg+max":
+            x1 = F.avg_pool2d(x, kernel_size=pool_size)
+            x2 = F.max_pool2d(x, kernel_size=pool_size)
+            x = x1 + x2
+        else:
+            raise Exception("Incorrect argument!")
+
+        return x
+
+
 class CNN_two(nn.Module):
-    def __init__(self, classes_num):
+    def __init__(self, classes_num, input_length, order1_length, order2_length):
 
         super(CNN_two, self).__init__()
 
-        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64, kernel_size=(5, 5))
-        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128, kernel_size=(5, 5))
-        
-        self.conv_block3 = ConvBlock(in_channels=1, out_channels=64, kernel_size=(1, 5))
-        self.conv_block4 = ConvBlock(in_channels=64, out_channels=128, kernel_size=(1, 5))
+        depth1 = min(
+            math.floor(math.log(order1_length) / math.log(2)) - 1,
+            math.floor(math.log(input_length) / math.log(2)) - 1,
+        )
+        depth2 = min(
+            math.floor(math.log(order2_length) / math.log(2)) - 1,
+            math.floor(math.log(input_length) / math.log(2)) - 1,
+        )
 
-        self.flatten = Flatten()
-        
-        self.fc1 = nn.Linear(577792, 128, bias=True)
-        self.bn0 = nn.BatchNorm1d(128)
-        self.fc_medley = nn.Linear(128, classes_num, bias=True)
+        self.conv_block1 = ConvBlock5x5(
+            in_channels=1, out_channels=32, kernel_size=(3, 3)
+        )
+        self.conv_list1 = nn.ModuleList(
+            [
+                ConvBlock5x5(
+                    in_channels=32 * 2 ** i,
+                    out_channels=32 * 2 ** (i + 1),
+                    kernel_size=(3, 3),
+                )
+                for i in range(depth1 - 1)
+            ]
+        )
+
+        self.conv_block2 = ConvBlock5x5(
+            in_channels=1, out_channels=32, kernel_size=(1, 3)
+        )
+        self.conv_list2 = nn.ModuleList(
+            [
+                ConvBlock5x5(
+                    in_channels=32 * 2 ** i,
+                    out_channels=32 * 2 ** (i + 1),
+                    kernel_size=(1, 3),
+                )
+                for i in range(depth2 - 1)
+            ]
+        )
+
+        self.fc1 = nn.Linear(
+            32 * 2 ** (depth1 - 1) + 32 * 2 ** (depth2 - 1), 512, bias=True
+        )
+        self.bn0 = nn.BatchNorm1d(512)
+        self.fc_medley = nn.Linear(512, classes_num, bias=True)
 
         self.init_weight()
 
@@ -100,19 +182,30 @@ class CNN_two(nn.Module):
 
         x2 = input2.view(input2.shape[0], 1, input2.shape[1], input2.shape[2])
 
-        x1 = self.conv_block1(x1, pool_size=(2, 2), pool_type='avg')
-        x1 = self.conv_block2(x1, pool_size=(2, 2), pool_type='avg')
-        
-        x2 = self.conv_block3(x2, pool_size=(2, 2), pool_type='avg')
-        x2 = self.conv_block4(x2, pool_size=(2, 2), pool_type='avg')
+        x1 = self.conv_block1(x1, pool_size=(2, 2), pool_type="avg")
 
-        x1 = self.flatten(x1)
-        x2 = self.flatten(x2)
+        for i, c in enumerate(self.conv_list1):
+            x1 = c(x1, pool_size=(2, 2), pool_type="avg")
+
+        x2 = self.conv_block2(x2, pool_size=(2, 2), pool_type="avg")
+
+        for i, c in enumerate(self.conv_list2):
+            x2 = c(x2, pool_size=(2, 2), pool_type="avg")
+
+        x1 = torch.mean(x1, dim=3)
+        (x11, _) = torch.max(x1, dim=2)
+        x12 = torch.mean(x1, dim=2)
+        x1 = x11 + x12
+
+        x2 = torch.mean(x2, dim=3)
+        (x21, _) = torch.max(x2, dim=2)
+        x22 = torch.mean(x2, dim=2)
+        x2 = x21 + x22
 
         x = torch.cat((x1, x2), 1)
 
         embedding = F.relu(self.fc1(x))
-        
+
         x = self.bn0(embedding)
 
         clipwise_output = torch.sigmoid(self.fc_medley(x))
@@ -120,52 +213,22 @@ class CNN_two(nn.Module):
         output_dict = {"clipwise_output": clipwise_output, "embedding": embedding}
 
         return output_dict
-    
-class ConvBlock5x5(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        
-        super(ConvBlock5x5, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=in_channels, 
-                              out_channels=out_channels,
-                              kernel_size=(5, 5), stride=(1, 1),
-                              padding=(2, 2), bias=False)
-                              
-        self.bn1 = nn.BatchNorm2d(out_channels)
 
-        self.init_weight()
-        
-    def init_weight(self):
-        init_layer(self.conv1)
-        init_bn(self.bn1)
 
-        
-    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
-        
-        x = input
-        x = F.relu_(self.bn1(self.conv1(x)))
-        if pool_type == 'max':
-            x = F.max_pool2d(x, kernel_size=pool_size)
-        elif pool_type == 'avg':
-            x = F.avg_pool2d(x, kernel_size=pool_size)
-        elif pool_type == 'avg+max':
-            x1 = F.avg_pool2d(x, kernel_size=pool_size)
-            x2 = F.max_pool2d(x, kernel_size=pool_size)
-            x = x1 + x2
-        else:
-            raise Exception('Incorrect argument!')
-        
-        return x
-    
 class Cnn6(nn.Module):
-    def __init__(self, classes_num, time_steps, freq_bins):
-        
+    def __init__(self, classes_num, time_steps, freq_bins, spec_aug=True):
+
         super(Cnn6, self).__init__()
 
         # Spec augmenter
-        self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
-            freq_drop_width=8, freq_stripes_num=2)
-        
+        self.spec_aug = spec_aug
+        self.spec_augmenter = SpecAugmentation(
+            time_drop_width=64,
+            time_stripes_num=2,
+            freq_drop_width=8,
+            freq_stripes_num=2,
+        )
+
         self.bn0 = nn.BatchNorm2d(freq_bins)
 
         self.conv_block1 = ConvBlock5x5(in_channels=1, out_channels=64)
@@ -175,36 +238,36 @@ class Cnn6(nn.Module):
 
         self.fc1 = nn.Linear(512, 512, bias=True)
         self.fc_openmic = nn.Linear(512, classes_num, bias=True)
-        
+
         self.init_weight()
 
     def init_weight(self):
         init_bn(self.bn0)
         init_layer(self.fc1)
         init_layer(self.fc_openmic)
- 
+
     def forward(self, input, mixup_lambda=None):
         """
         Input: (batch_size, data_length)"""
 
-        x = input[:,0,:,:]
+        x = input[:, 0, :, :]
         x = x.transpose(1, 3)
         x = self.bn0(x)
         x = x.transpose(1, 3)
-        
-        if self.training:
+
+        if self.training and self.spec_aug:
             x = self.spec_augmenter(x)
-        
-        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
+
+        x = self.conv_block1(x, pool_size=(2, 2), pool_type="avg")
         x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
+        x = self.conv_block2(x, pool_size=(2, 2), pool_type="avg")
         x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
+        x = self.conv_block3(x, pool_size=(2, 2), pool_type="avg")
         x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
+        x = self.conv_block4(x, pool_size=(2, 2), pool_type="avg")
         x = F.dropout(x, p=0.2, training=self.training)
         x = torch.mean(x, dim=3)
-        
+
         (x1, _) = torch.max(x, dim=2)
         x2 = torch.mean(x, dim=2)
         x = x1 + x2
@@ -212,7 +275,7 @@ class Cnn6(nn.Module):
         x = F.relu_(self.fc1(x))
         embedding = F.dropout(x, p=0.5, training=self.training)
         clipwise_output = torch.sigmoid(self.fc_openmic(x))
-        
-        output_dict = {'clipwise_output': clipwise_output, 'embedding': embedding}
+
+        output_dict = {"clipwise_output": clipwise_output, "embedding": embedding}
 
         return output_dict
