e392101507518c49adfd8f3245b4a46cf66c097c
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-06 13:02:26 +0000

    multiprocessing

--- two_inputs/create_mel.py
+++ two_inputs/create_mel.py
@@ -6,7 +6,9 @@ import librosa
 import IPython.display as ipd
 import matplotlib.pyplot as plt
 from stft import Spectrogram, LogmelFilterBank
-from multiprocessing import Process
+import multiprocessing as mp
+import math
+from tqdm import tqdm
 
 sample_rate = 22050
 window_size = 400
@@ -64,16 +66,22 @@ def get_logmel(x):
     return x
 
 
-scatter_type = "9_8_132300"
+scatter_type = "9_8_132300_reduced"
 
 scatter_path = "/home/laura/MedleyDB/processed/" + scatter_type
-logmel_path = "/home/laura/MedleyDB/processed/logmel"
+logmel_path = "/home/laura/MedleyDB/processed/logmel_reduced"
+
+if not os.path.exists("/home/laura/MedleyDB/processed/logmel_reduced/input/"):
+    os.mkdir("/home/laura/MedleyDB/processed/logmel_reduced/input/")
+
+if not os.path.exists("/home/laura/MedleyDB/processed/logmel_reduced/labels/"):
+    os.mkdir("/home/laura/MedleyDB/processed/logmel_reduced/labels/")
 
 
 def f(files):
     save_path = os.path.join(logmel_path, "input")
 
-    for file in files:
+    for file in tqdm(files):
         sample_name = file[:-4]
 
         start = int(sample_name.split("_")[2]) * 22050
@@ -85,15 +93,30 @@ def f(files):
         audio_path = os.path.join("/home/laura/MedleyDB/Audio/", wav_name)
 
         y, _ = librosa.load(audio_path, sr=22050, res_type="kaiser_fast")
-        waveform = y[start : start + 6 * 22050]
+        waveform = y[start : start + int(scatter_type.split("_")[2])]
 
         logmel = get_logmel(waveform)
 
         np.save(os.path.join(save_path, file), logmel)
 
 
+def divide_chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
 if __name__ == "__main__":
+    processes = []
+    num_cpu = mp.cpu_count()
+
     files = os.listdir(os.path.join(scatter_path, "input/"))
-    p = Process(target=f, args=(files,))
-    p.start()
-    p.join()
+    files = list(divide_chunks(files, math.ceil(len(files) / num_cpu)))
+
+    for i in range(num_cpu):
+        p = mp.Process(target=f, args=(files[i],))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
