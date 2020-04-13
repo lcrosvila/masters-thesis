7e3f0cdb8e28f9b38a5e5c4ac7a03a0941237b42
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-13 12:09:07 +0000

    add instrumentwise precision recall

--- two_inputs/metrics.py
+++ two_inputs/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 
 def macro_f1(batches_target, batches_pred):
@@ -17,6 +17,26 @@ def instrument_f1(batches_target, batches_pred):
         instrument_f1.append(f1_score(target_instr, pred_instr))
 
     return instrument_f1
+
+
+def instrument_precision(batches_target, batches_pred):
+    instrument_precision = []
+    for i in range(len(batches_target[0])):
+        target_instr = [sample[i] for sample in batches_target]
+        pred_instr = [sample[i] for sample in batches_pred]
+        instrument_precision.append(precision_score(target_instr, pred_instr))
+
+    return instrument_precision
+
+
+def instrument_recall(batches_target, batches_pred):
+    instrument_recall = []
+    for i in range(len(batches_target[0])):
+        target_instr = [sample[i] for sample in batches_target]
+        pred_instr = [sample[i] for sample in batches_pred]
+        instrument_recall.append(recall_score(target_instr, pred_instr))
+
+    return instrument_recall
 
 
 def auc_score():
