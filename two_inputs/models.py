e1e45409473cbcad2ea69b92cf3461026cead10c
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-30 08:05:22 +0000

    remove unnecesary functions

--- two_inputs/models.py
+++ two_inputs/models.py
@@ -23,62 +23,6 @@ def init_bn(bn):
     bn.weight.data.fill_(1.0)
 
 
-class Flatten(nn.Module):
-    def forward(self, input):
-        return input.view(input.size(0), -1)
-
-
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size):
-
-        super(ConvBlock, self).__init__()
-
-        self.conv1 = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=(1, 1),
-            padding=(1, 1),
-            bias=False,
-        )
-
-        self.conv2 = nn.Conv2d(
-            in_channels=out_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=(1, 1),
-            padding=(1, 1),
-            bias=False,
-        )
-
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-
-        self.init_weight()
-
-    def init_weight(self):
-        init_layer(self.conv1)
-        init_layer(self.conv2)
-
-    def forward(self, input, pool_size=(2, 2), pool_type="avg"):
-
-        x = input
-        x = F.relu_(self.bn1(self.conv1(x)))
-        x = F.relu_(self.bn2(self.conv2(x)))
-        if pool_type == "max":
-            x = F.max_pool2d(x, kernel_size=pool_size)
-        elif pool_type == "avg":
-            x = F.avg_pool2d(x, kernel_size=pool_size)
-        elif pool_type == "avg+max":
-            x1 = F.avg_pool2d(x, kernel_size=pool_size)
-            x2 = F.max_pool2d(x, kernel_size=pool_size)
-            x = x1 + x2
-        else:
-            raise Exception("Incorrect argument!")
-
-        return x
-
-
 class ConvBlock5x5(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=(5, 5)):
 
